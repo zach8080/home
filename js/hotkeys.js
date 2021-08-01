8f4176baa0558937bf0ec634ca68b18a6991c845
zach8080 <zachary.dawood@gmail.com>
2021-08-01 14:06:55 -0400

    changed links

--- js/hotkeys.js
+++ js/hotkeys.js
@@ -1,130 +1,144 @@
 window.addEventListener('keydown',(event)=>{
     if (event.key === 'g' ) {
         event.preventDefault();
-        window.location.href = "http://github.com";
+	window.open('https://github.com', '_blank').focus();
     }
 })
 window.addEventListener('keydown',(event)=>{
     if (event.key === 'p' ) {
         event.preventDefault();
-        window.location.href = "http://protonmail.com";
+        window.open('https://protonmail.com', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'a' ) {
+        event.preventDefault();
+        window.open('https://www.acorn.utoronto.ca/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'u' ) {
+        event.preventDefault();
+        window.open('https://q.utoronto.ca/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'm' ) {
+        event.preventDefault();
+        window.open('https://mail.google.com/mail/u/0/#inbox', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'o' ) {
+        event.preventDefault();
+        window.open('https://outlook.office365.com', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'y' ) {
+        event.preventDefault();
+        window.open('https://youtube.com', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 't' ) {
+        event.preventDefault();
+        window.open('https://boards.4channel.org/g/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'b' ) {
+        event.preventDefault();
+        window.open('https://boards.4channel.org/biz/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'f' ) {
+        event.preventDefault();
+        window.open('https://last.fm', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'i' ) {
+        event.preventDefault();
+        window.open('https://www.reddit.com/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'd' ) {
+        event.preventDefault();
+        window.open('https://discord.com/channels/@me', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 's' ) {
+        event.preventDefault();
+        window.open('https://m.me', '_blank').focus();
     }
 })
 window.addEventListener('keydown',(event)=>{
     if (event.key === 'c' ) {
         event.preventDefault();
-        window.location.href = "https://coolors.co/";
+        window.open('https://my.pcloud.com/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'v' ) {
+        event.preventDefault();
+        window.open('https://drive.google.com/drive/u/0/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'j' ) {
+        event.preventDefault();
+        window.open('https://thepiratebay.org/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'n' ) {
+        event.preventDefault();
+        window.open('https://b-ok.cc/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'h' ) {
+        event.preventDefault();
+        window.open('https://the-eye.eu/public/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'q' ) {
+        event.preventDefault();
+        window.open('https://my.questrade.com/trading/', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'k' ) {
+        event.preventDefault();
+        window.open('https://www.kraken.com/u/trade', '_blank').focus();
+    }
+})
+window.addEventListener('keydown',(event)=>{
+    if (event.key === 'w' ) {
+        event.preventDefault();
+        window.open('https://www.yieldwatch.net/', '_blank').focus();
     }
 })
 window.addEventListener('keydown',(event)=>{
     if (event.key === 'x' ) {
         event.preventDefault();
-        window.location.href = "https://pixlr.com/x/";
+        window.open('https://finviz.com/', '_blank').focus();
     }
 })
 window.addEventListener('keydown',(event)=>{
-    if (event.ctrlKey && event.key === 'r' ) {
+    if (event.key === 'z' ) {
         event.preventDefault();
-        window.location.href = "https://repl.it/";
+        window.open('https://zacharydawood.xyz/', '_blank').focus();
     }
 })
 window.addEventListener('keydown',(event)=>{
-    if (event.key === '9') {
+    if (event.key === 'e' ) {
         event.preventDefault();
-        window.location.href = "https://9animes.ru/";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'y') {
-        event.preventDefault();
-        window.location.href = "https://yewtu.be/";
-    }
-})
-
-window.addEventListener('keydown',(event)=>{
-    if (event.key === '4') {
-        event.preventDefault();
-        window.location.href = "https://4chan.org/";
-    }
-})
-
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'f') {
-        event.preventDefault();
-        window.location.href = "https://www.fushaar.com";
-    }
-})
-
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'm') {
-        event.preventDefault();
-        window.location.href = "https://mega.nz";
-    }
-})
-
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'u') {
-        event.preventDefault();
-        window.location.href = "https://www.unitconverters.net";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'a') {
-        event.preventDefault();
-        window.location.href = "https://www.aspose.app/";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 't') {
-        event.preventDefault();
-        window.location.href = "http://thepiratebay.org/index.html";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === '1') {
-        event.preventDefault();
-        window.location.href = "https://1337x.to";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'w') {
-        event.preventDefault();
-        window.location.href = "https://www.wunderground.com/";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'r') {
-        event.preventDefault();
-        window.location.href = "https://www.reddit.com/";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.ctrlKey && event.key === 'f') {
-        event.preventDefault();
-        window.location.href = "https://www.facebook.com";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.key === 'd') {
-        event.preventDefault();
-        window.location.href = "https://www.discord.com";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.ctrlKey && event.key === 'm') {
-        event.preventDefault();
-        window.location.href = "https://m.me";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.ctrlKey && event.key === 't') {
-        event.preventDefault();
-        window.location.href = "https://twitter.com";
-    }
-})
-window.addEventListener('keydown',(event)=>{
-    if (event.shiftKey && event.key === 'R') {
-        event.preventDefault();
-        window.location.href = "https://ruqqus.com";
+        window.open('https://mirrorace.com/', '_blank').focus();
     }
 })
